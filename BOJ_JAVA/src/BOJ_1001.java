632686deb1d153fbbbc035e754e86ad19d4c8698
kimnoca <kimnoca@naver.com>
2022-07-22 19:27:11 +0900

    백준 1001

---
+++ BOJ_JAVA/src/BOJ_1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+//BOJ 1001 A-B ¹®Á¦
+public class BOJ_1001 {
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int ans = num1 - num2;
+		System.out.println(ans);
+	}
+}
