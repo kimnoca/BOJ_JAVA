dd2641233d4a1f8025239edeee976be73f2f3297
kimnoca <kimnoca@naver.com>
2023-01-05 23:55:06 +0900

    BOJ-2558

--- BOJ_2558.java
+++ BOJ_2558.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class BOJ_2558 {
 
 	public static void main(String[] args) {
-		int a;
-		int b;
+		int a, b;
 		Scanner sc = new Scanner(System.in);
 		a = sc.nextInt();
 		b = sc.nextInt();
