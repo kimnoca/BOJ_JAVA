4069cf8b9261ed9c3f8a537076c433d7438476e1
kimnoca <kimnoca@naver.com>
2023-01-05 23:13:38 +0900

    1021

--- BOJ_1021.java
+++ BOJ_1021.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class BOJ_1021 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N, M ;
-		N = sc.nextInt();
-		M = sc.nextInt();
+		//아직 못품
+		
 	}
 
 }
